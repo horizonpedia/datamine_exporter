032c3d0c78b5ce75be58e8760f1af0a433c4a0cf
panicbit <panicbit.dev@gmail.com>
2020-05-30 21:31:18 +0200

    move datamine model into submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use anyhow::*;
-use serde::Deserialize;
+
+mod datamine;
+pub use datamine::*;
 
 const DATAMINE_PATH: &str = "datamine.json";
 
@@ -12,75 +14,4 @@ pub fn read_datamine() -> Result<DataMine> {
         .context("deserializing datamine")?;
 
     Ok(datamine)
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct DataMine {
-    pub sheets: Vec<Sheet>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct Sheet {
-    pub properties: SheetProperties,
-    pub data: Vec<GridData>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct SheetProperties {
-    pub title: String,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct GridData {
-    pub row_data: Vec<RowData>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct RowData {
-    pub values: Vec<CellData>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all="camelCase")]
-pub struct CellData {
-    pub user_entered_value: Option<ExtendedValue>,
-    pub effective_value: Option<ExtendedValue>,
-}
-
-// #[derive(Deserialize, Debug)]
-// #[serde(rename_all="camelCase")]
-// pub struct ExtendedValue {
-//     number_value: Option<f64>,
-//     // bool_value: Option<bool>,
-//     // // error_value: Option<f64>,
-//     // formula_value: Option<String>,
-//     // string_value: Option<String>,
-// }
-
-#[derive(Deserialize, Debug)]
-#[serde(untagged)]
-pub enum ExtendedValue {
-    Number {
-        #[serde(rename="numberValue")]
-        value: f64,
-    },
-    String {
-        #[serde(rename="stringValue")]
-        value: String,
-    },
-    Bool {
-        #[serde(rename="boolValue")]
-        value: bool,
-    },
-    Formula {
-        #[serde(rename="formulaValue")]
-        value: String,
-    },
-    // error_value: Option<ErrorValue>,
-    Empty{},
 }
